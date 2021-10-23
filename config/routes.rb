cee5b61180c05d209974398c5a317bb294bf3da4
murazawa <murazawa>
2021-10-23 16:50:35 +0000

    usersコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'users/index'
+  get 'users/show'
   root 'homes#top'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
