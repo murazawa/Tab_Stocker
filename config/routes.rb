745afab4a0ad694bd3671fc5a72d276901f8d727
murazawa <murazawa>
2021-10-23 16:55:16 +0000

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   
-  get 'users/index'
-  get 'users/show'
   root 'homes#top'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'   
   }
-  
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
 
 end
