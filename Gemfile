4434c4099811a37d9c97f6411361c39c34eef8f7
murazawa <mura.hi0115ki@gmail.com>
2021-10-29 15:39:09 +0000

    bootstrap導入

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'ransack'
 gem 'acts-as-taggable-on'
 gem 'kaminari'
 gem 'devise-i18n'
+gem 'webpacker', '~> 4.0'
+gem 'bootstrap'
 
 group :development, :test do
   gem 'byebug', platform: :mri
---
+++ config/webpack/production.js
@@ -0,0 +1,5 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+
+const environment = require('./environment')
+config.assets.js_compressor = :uglifier
+module.exports = environment.toWebpackConfig()
