2d993f00386b8474ef2ceda55acf239f16424705
murazawa <mura.hi0115ki@gmail.com>
2021-11-15 08:35:02 +0000

    いいね機能 非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,12 @@ class FavoritesController < ApplicationController
     my_link = MyLink.find(params[:my_link_id])
     favorite = current_user.favorites.new(my_link_id: my_link.id)
     favorite.save
-    redirect_to my_link_path(my_link)
   end
   
   def destroy
     my_link = MyLink.find(params[:my_link_id])
     favorite = current_user.favorites.find_by(my_link_id: my_link.id)
     favorite.destroy
-    redirect_to  my_link_path(my_link)
   end
 end
   # post_image = PostImage.find(params[:post_image_id])
