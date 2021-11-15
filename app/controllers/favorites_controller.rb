10de1a68dd9fd4c799c8d331f13e271b5ed542f3
murazawa <mura.hi0115ki@gmail.com>
2021-11-15 09:02:17 +0000

    いいね機能　非同期 実装完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
   
   def create
-    my_link = MyLink.find(params[:my_link_id])
-    favorite = current_user.favorites.new(my_link_id: my_link.id)
+    @my_link = MyLink.find(params[:my_link_id])
+    favorite = current_user.favorites.new(my_link_id: @my_link.id)
     favorite.save
   end
   
   def destroy
-    my_link = MyLink.find(params[:my_link_id])
-    favorite = current_user.favorites.find_by(my_link_id: my_link.id)
+    @my_link = MyLink.find(params[:my_link_id])
+    favorite = current_user.favorites.find_by(my_link_id: @my_link.id)
     favorite.destroy
   end
 end
