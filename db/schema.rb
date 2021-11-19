16ed7772a2f49c16c26630be12abfc1832d598ff
murazawa <mura.hi0115ki@gmail.com>
2021-11-19 13:30:11 +0000

    db修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_132157) do
+ActiveRecord::Schema.define(version: 2021_11_18_085901) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_11_18_132157) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "tab_collection_id"
+    t.integer "my_link_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
